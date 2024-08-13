cf3dbd5bca8ac31df59f73d7824bde1615386021
Murrell Johnson <dev@klikz.us>
2024-08-13 03:00:42 -0400

    chore: script

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-# Clean up objects - run 3 times each in case of errors.
 py manage.py delete orders
 py manage.py delete orders
 py manage.py delete orders
@@ -9,12 +8,10 @@ py manage.py delete products
 py manage.py delete products
 py manage.py delete products
 
-# Read data
 py manage.py read customers
 py manage.py read products
 py manage.py read orders
 
-# Upload objects - run 3 times each in case of errors.
 py manage.py sync products
 py manage.py sync products
 py manage.py sync products
