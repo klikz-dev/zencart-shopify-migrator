7ce5cf8798648251de2c7fec2d9b23d6f088377f
Murrell Johnson <dev@klikz.us>
2024-10-24 22:36:05 +0000

    feat: order shipments

--- vendor/management/commands/export.py
+++ vendor/management/commands/export.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from django.core.management.base import BaseCommand
 from collections import defaultdict
 import csv
-from vendor.models import Vendor, PurchaseOrder, PurchaseOrderDetail
+from vendor.models import Vendor, Order, PurchaseOrderDetail
 
 FILEDIR = f"{Path(__file__).resolve().parent.parent}/files"
 
@@ -21,6 +21,9 @@ class Command(BaseCommand):
 
         if "purchase-orders" in options['functions']:
             processor.purchase_orders()
+
+        if "shipment" in options['functions']:
+            processor.order_shipments()
 
 
 class Processor:
@@ -73,7 +76,7 @@ class Processor:
             "po": "",
             "supplier": "",
             "warehouse": "Default Warehouse",
-            "store": "Shopify",
+            "store": "VinsRare Shopify",
             "quantity": 0,
             "rate": 0,
             "sku": "",
@@ -118,5 +121,68 @@ class Processor:
             ])
 
         with open(f"{FILEDIR}/purchase-orders.csv", mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+    def order_shipments(self):
+        data = []
+        data.append([
+            "Store",
+            "Order #",
+            "Carrier",
+            "Service",
+            "Tracking #",
+            "Cost",
+            "SKU",
+            "Quantity",
+            "Warehouse",
+        ])
+
+        # Create a dictionary to hold combined results
+        combined_data = defaultdict(lambda: {
+            "store": "VinsRare Shopify",
+            "po": "",
+            "carrier": "Custom",
+            "service": "",
+            "tracking": "#",
+            "cost": 0,
+            "sku": "",
+            "quantity": 0,
+            "warehouse": "Default Warehouse",
+        })
+
+        orders = Order.objects.all().filter(order_id=41639)
+        for order in orders:
+            lineItems = order.lineItems.all()
+            for lineItem in lineItems:
+                sku = lineItem.product.product_id
+                quantity = lineItem.shipped
+                
+                if sku is not None and quantity > 0:
+                    key = (order.order_id, sku)
+                    if combined_data[key]['po'] == "":
+                        combined_data[key].update({
+                            "po": order.order_id,
+                            "service": order.shipping_method,
+                            "cost": order.shipping_price,
+                            "sku": sku,
+                            "quantity": quantity,
+                        })
+
+
+        for item in combined_data.values():
+            data.append([
+                item['store'],
+                item['po'],
+                item['carrier'],
+                item['service'],
+                item['tracking'],
+                item['cost'],
+                item['sku'],
+                item['quantity'],
+                item['warehouse'],
+            ])
+
+        with open(f"{FILEDIR}/order-shipments.csv", mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(data)
