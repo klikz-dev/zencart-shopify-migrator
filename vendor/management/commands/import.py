35d8ed30000dba8e28c4bb634d2b8509224c7293
Murrell Johnson <dev@klikz.us>
2024-12-18 21:27:56 +0000

    chore: update import script

--- vendor/management/commands/import.py
+++ vendor/management/commands/import.py
@@ -1,4 +1,1 @@
-# Win + R
-# cmd
-# cd c://xampp/mysql/bin
-# mysql - u root -p --default-character-set=utf8 vinsrare < "D:\projects\vinsrare\zencart-shopify-migrator\vendor\management\files\source.sql"
+# mysql -u root -p --default-character-set=utf8 vinsrare --force < vendor/management/files/source.sql
